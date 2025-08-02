0116d084baadc106b81e63ba9fe92f3d9a5a9d69
desarrollo <desarrollo@academicos.cesde.edu.co>
2025-08-01 20:21:19 -0500

    se organiza en carpetas

---
+++ intellij/untitled/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+import Scanner.java;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("Proyecto de practica");
+
+            int nro1,nro2;
+        
+    }
+}
